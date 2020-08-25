61d346ee5169129a82cf29e32985bba4fea8f3c1
Bruno Andreotti <bruno.andreotti@me.com>
2020-08-25 11:16:18 -0300

    Teste luz e foreground

--- ProjectAbyss-1/ProjectAbyss/Assets/Scripts/PlayerMovement.cs
+++ ProjectAbyss-1/ProjectAbyss/Assets/Scripts/PlayerMovement.cs
@@ -2,49 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+namespace Assets.Scripts
 {
-    public CharacterController2D controller;
-    public Animator animator;
-
-    public float runSpeed = 40f;
-    float HorizontalMove = 0f;
-    float jump = 0f;
-
-    // Update is called once per frame
-    void Update()
+    public class PlayerMovement : MonoBehaviour
     {
-        // GetComponent<Grab>().isHolding = true;
-        
-            
+        public CharacterController2D controller;
+        public Animator animator;
+
+        public float runSpeed = 40f;
+        float HorizontalMove = 0f;
+        bool jump = false;
+
+        // Update is called once per frame
+        void Update()
+        {
+            // GetComponent<Grab>().isHolding = true;
             HorizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-       
-        //animator.SetFloat("Speed",Mathf.Abs(HorizontalMove));
 
-        if(Input.GetAxisRaw("Vertical")> 0)
-        {
-            jump = 20f;
-            animator.SetBool("IsJumping", true);
-        }
-       /* if (GetComponent<Grab>().isHolding == true)
-        {
-            animator.SetBool("IsGrabbing", true);
-        }
-        else
-        {
-            animator.SetBool("IsGrabbing", false);
-        }
-        */
-    }
+            //animator.SetFloat("Speed", Mathf.Abs(HorizontalMove));
 
-    public void OnLanding()
-    {
-        animator.SetBool("IsJumping", false);
-    }
-    private void FixedUpdate()
-    {
-        //move character
-        controller.Move(HorizontalMove * Time.fixedDeltaTime, false, jump);
-        jump = 0f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                Debug.Log("Jumped");
+                jump = true;
+                animator.SetBool("IsJumping", true);
+            }
+            /*
+            if (GetComponent<Grab>().isHolding == true)
+            {
+                animator.SetBool("IsGrabbing", true);
+            }
+            else
+            {
+                animator.SetBool("IsGrabbing", false);
+            }*/
+        }
+
+        public void OnLanding()
+        {
+            animator.SetBool("IsJumping", false);
+        }
+        private void FixedUpdate()
+        {
+            //move character
+            controller.Move(HorizontalMove * Time.fixedDeltaTime, false, jump);
+            jump = false;
+        }
     }
 }
