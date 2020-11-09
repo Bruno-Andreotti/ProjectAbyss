3c2adbb229996617d8d6df7cdfd7cf47ea2e655c
Bruno Andreotti <bruno.andreotti@me.com>
2020-11-09 20:16:52 -0300

    Revamp do Menu Principal

---
+++ ProjectAbyss-1/ProjectAbyss/Assets/Scripts/NPCSelect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSelect : MonoBehaviour
+{
+    public float NPCID;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator.SetFloat("NPC", NPCID);
+    }
+
+    // Update is called once per frame
+    
+}
